63a914f4b1058a20280774fa3b0b2e76f4e1b01e
krsnamara <martinj.fitzpatrick@gmail.com>
2024-01-15 15:27:38 -0500

    dynamic routing working, back navigation on details page

--- app/addRock/[rockId]/page.tsx
+++ app/addRock/[rockId]/page.tsx
@@ -1,21 +1,39 @@
 'use client'
+import { useRouter } from 'next/navigation'
 import React from 'react'
 import { useRockContext } from '@/app/context/RockContext'
+import { Button } from '@/components/elements/Button'
 
-function Details({ params }: { params: { rockId: string } }) {
+function Details({ params }: { params: { rockId: number } }) {
   const { rocks } = useRockContext()
 
-  const selectedRock = rocks.find((rock) => rock.id === params.rockId)
+  // console.log(rocks)
+
+  // Find the rock with the id that matches the params.rockId
+  const selectedRock = rocks.find((rock) => rock.id == params.rockId)
+
+  // console.log(selectedRock)
+
+  const router = useRouter()
+
+  const goBack = () => {
+    router.back()
+  }
 
   return (
     <>
-      <div>
-        <h1>Rock details about {params.rockId}</h1>
-      </div>
+      <div>{/* <h1>Rock details about {params.rockId}</h1> */}</div>
       <div>
         {selectedRock ? (
           <div>
             <h1>Rock details about {selectedRock.name}</h1>
+            <Button
+              className="col-span-2 w-52 place-self-center"
+              onClick={goBack}
+            >
+              Go Back
+            </Button>
+
             {/* Display other details of the rock */}
           </div>
         ) : (
